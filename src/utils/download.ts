b988f97f7e71a78fbef0772f18cdbe36120ea6f8
IliaKova <ilia.kovalenko@remme.io>
2023-04-30 17:12:37 +0300

    Updates

--- src/utils/download.ts
+++
@@ -1,7 +0,0 @@
-export const download = (content: string, fileName: string, contentType: string) => {
-    const a = document.createElement("a");
-    const file = new Blob([content], { type: contentType });
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-}
